ec5cd1776d2dd718a9f8cbed6bab1d284cb3d2db
Ronald Huidrom <ronhuidrom@gmail.com>
2022-10-16 22:29:57 +0530

    Add files via upload

--- C++14/glambdas.cpp
+++ C++14/glambdas.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <iostream>
+#include <functional> // for std::function
 
 using namespace std;
 
@@ -19,5 +20,14 @@ int main(){
   cout << add(8.2, 4) << endl; // return int
   cout << add(3, 4.2) << endl; // return double
 
+  // explicitly specifying return type
+  auto multiply = [](auto a, auto b) -> int { return a*b; };
+
+  // std::function allows to declare signature of a lambda before defining it as lambda
+  std::function<int(int)> CountOnes; // signature
+  CountOnes = [&CountOnes](int n) -> int { return (0==n) ? 0 : CountOnes(n/2) + (n%2); };
+  cout << CountOnes(12) << endl;
+
+  
   return 0;
 }
