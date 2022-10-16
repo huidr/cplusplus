a08a6a3d30e11a58757594bac1206affc1b3d988
Ronald Huidrom <ronhuidrom@gmail.com>
2022-10-16 22:16:38 +0530

    Add files via upload

--- C++14/glambdas.cpp
+++ C++14/glambdas.cpp
@@ -8,8 +8,16 @@ using namespace std;
 
 int main(){
 
+  // using auto
   auto adder = [](auto a, auto b) {return a + b; };
-  cout << adder(4.2, 7) << endl;
+  cout << adder(4, 6) << endl;
+  cout << adder(4.4, 6.2) << endl;
+
+  // using decltype
+  auto add = [](auto a, auto b) -> decltype(b) { return a + b; };
+  cout << add(4, 2) << endl; // return int
+  cout << add(8.2, 4) << endl; // return int
+  cout << add(3, 4.2) << endl; // return double
 
   return 0;
 }
